9d84fda76cef3ba32a6bf22ad7987953441c206c
ssboc0618 <oshinko0618@yahoo.co.jp>
2021-08-06 23:14:40 +0900

    commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages = TextMessage(text='おはようございます \n朝です、起きてください')
+    messages = TextMessage(text='おはようございます! \n朝ですよ!起きてください!')
     line_bot_api.push_message(USER_ID, messages=messages)
 
 if __name__ == '__main__':
